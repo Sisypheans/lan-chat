0772beb72407492be58563cd0fe6512a9998f3f5
liuzh <liuzhenyu_work@163.com>
2021-09-25 23:28:00 +0800

    增加组播接口

--- src/main/server/index.js
+++ src/main/server/index.js
@@ -3,29 +3,68 @@ import app from './server'
 import http from 'http'
 import config from '@config'
 const port = config.BuiltInServerPort
-var server = null
+// var server = null
 app.set('port', port)
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+// const local_ip = "192.168.0.xx";
+const multicast_ip = "225.0.0.100";  // 这里必须是一个组播地址(D类IP地址)
+
 
 export default {
   StatrServer () {
     return new Promise((resolve, reject) => {
-      server = http.createServer(app)
-      server.listen(port)
-      server.on('error', (error) => {
-        switch (error.code) {
-          case 'EACCES':
-            reject('权限不足内置服务器启动失败，请使用管理员权限运行。')
-            break
-          case 'EADDRINUSE':
-            reject('内置服务器端口已被占用，请检查。')
-            break
-          default:
-            reject(error)
-        }
-      })
-      server.on('listening', () => {
-        resolve('服务端运行中')
-      })
+      // server = http.createServer(app)
+      // server.listen(port)
+      // server.on('error', (error) => {
+      //   switch (error.code) {
+      //     case 'EACCES':
+      //       reject('权限不足内置服务器启动失败，请使用管理员权限运行。')
+      //       break
+      //     case 'EADDRINUSE':
+      //       reject('内置服务器端口已被占用，请检查。')
+      //       break
+      //     default:
+      //       reject(error)
+      //   }
+      // })
+      // server.on('listening', () => {
+      //   resolve('服务端运行中')
+      // })
+      server.on('close', ()=>{
+        console.log('close socket');
+    });
+    
+    server.on('listening', ()=>{
+        console.log('listening...');
+        server.setBroadcast(true);
+        server.setMulticastTTL(128);
+        server.addMembership(multicast_ip);
+    });
+    
+    server.on('message', (msg, rinfo)=>{
+        console.log(`receive client message from ${rinfo.address}: ${rinfo.port}: ${msg}`);
+    });
+    
+    server.bind();  // 随机绑定本机一个端口
+    let interfaces = require('os').networkInterfaces();
+      for (var devName in interfaces) {
+          var iface = interfaces[devName];
+          for (var i = 0; i < iface.length; i++) {
+              let alias = iface[i];
+              if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal && alias.address.startsWith('192')) {
+                  // console.log(alias.address);
+    
+                  setInterval(()=>{
+                    server.send('hi, im server ' + alias.address, '8062', multicast_ip);
+                }, 5000);
+              }
+          }
+      }
+    
+      resolve('服务端运行中')
     })
   },
   StopServer () {
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -188,11 +188,36 @@ export default {
       ipcRenderer.invoke("open-win", data);
     },
     getMessage() {
-      message().then((res) => {
-        this.$alert(res.data, "提示", {
-          confirmButtonText: "确定",
-        });
+      // message().then((res) => {
+      //   this.$alert(res.data, "提示", {
+      //     confirmButtonText: "确定",
+      //   });
+      // });
+      const dgram = require('dgram');
+      var client = dgram.createSocket('udp4');
+
+      // const local_ip = '192.168.0.xx';
+      const multicast_ip = "225.0.0.100";   // 这里必须是一个组播地址(D类IP地址)
+
+      client.on('close', ()=>{
+          console.log('client closed');
       });
+
+      client.on('error', (err) =>{
+          console.log('client error' + err);
+      });
+
+      client.on('listening', ()=>{
+          console.log('client listening...');
+          client.setBroadcast(true);
+          client.setMulticastTTL(128);
+          client.addMembership(multicast_ip);
+      });
+
+      client.on('message', (msg, rinfo) => {
+          console.log(`receive server message from ${rinfo.address}: ${rinfo.port}: ${msg}`);
+      });
+      client.bind('8062');  // 监听组播数据的端口
     },
     StopServer() {
       ipcRenderer.invoke("stop-server").then((res) => {
